727a590c62270c3b07b6f45e8d6f4d67144aa089
Thiago Yure Vieira de Almeida <thigoyure@gmail.com>
2018-02-23 23:20:44 -0300

    Adcionando controllers e dois algoritmos de criptografia

---
+++ src/main/java/contole/SairController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package contole;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import modelo.Command;
+
+/**
+ *
+ * @author ThigoYure
+ */
+class SairController implements Command{
+
+    @Override
+    public void execute(HttpServletRequest req, HttpServletResponse res) throws SQLException, ClassNotFoundException, IOException, ServletException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
